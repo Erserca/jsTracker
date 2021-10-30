345d268476ec68cd8391a65a0bf97de50ed3b593
erserca <e.serhat.caliskan@gmail.com>
2021-10-30 16:08:33 +0300

    ...

--- index.php
+++ index.php
@@ -20,41 +20,4 @@ echo "<title>ESC| Erdoğan</title>";
 <script type="text/javascript" src="canvas.js"></script>
 </body>
 
-<script>
-/*function tikla(){
-
-var zindex = 15;
-var canvas = document.createElement("canvas");
-
-  function resizeCanvas() {
-
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-  }
-
-window.addEventListener('resize', resizeCanvas, false);
-canvas.className = 'canvases';
-canvas.style.border = '2px solid red';
-canvas.style.zIndex = zindex;
-zindex++;
-var parentDiv = document.getElementById("editor-section");
-parentDiv.appendChild(canvas);
-var ctx = canvas.getContext("2d");
-ctx.beginPath();
-var x = 0;
-var y = 0;
-ctx.moveTo(x, y);
-
-for(let i = 1; i< 50;i++){
-
-        x = Math.random() * window.innerWidth;
-        y =  Math.random() * window.innerHeight;
-        ctx.lineTo(x,y);
-        ctx.strokeStyle  = "#FF0000";
-        ctx.stroke();
-    }
-}
-*/
-</script>
 </html>
